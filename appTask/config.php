4195526f404f852280eb21d062a969375f299e2b
Gvelasquez100 <112146352+Gvelasquez100@users.noreply.github.com>
2024-05-22 21:22:20 -0600

    Update config.php

--- appTask/config.php
+++ appTask/config.php
@@ -1,7 +1,7 @@
 <?php
     
     // Variables de la conexion a la DB
-    $mysqli = new mysqli("roundhouse.proxy.rlwy.net:52958","root","XGeQjkoGJZdFCHtGrbrCYCcZDzVWZmpT","db_gestor");
+    $mysqli = new mysqli("roundhouse.proxy.rlwy.net:19772","root","eHfVzDXXCijxiTwUdbBcagjjthtdUzm","db_gestor");
     
     // Comprobamos la conexion
     if($mysqli->connect_errno) {
