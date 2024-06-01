23e5646b429d17124390245e5cec89eedff86764
Gvelasquez100 <112146352+Gvelasquez100@users.noreply.github.com>
2024-06-01 07:00:20 -0600

    Update config_bd.php

--- appTask/config_bd.php
+++ appTask/config_bd.php
@@ -1,8 +1,8 @@
 <?php
     
     // Variables de la conexion a la DB
-    $mysqli = new mysqli("127.0.0.1:3306","root","root","db_gestor"); //pruebas
-    //$mysqli = new mysqli("monorail.proxy.rlwy.net:37979","root","QYknGkzhCrHRctLkLrnLyeZmjXdBssGR","db_gestor"); //servidor
+    //$mysqli = new mysqli("127.0.0.1:3306","root","root","db_gestor"); //pruebas
+    $mysqli = new mysqli("monorail.proxy.rlwy.net:37979","root","QYknGkzhCrHRctLkLrnLyeZmjXdBssGR","db_gestor"); //servidor
     
     // Comprobamos la conexion
     if($mysqli->connect_errno) {
